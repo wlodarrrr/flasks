59c2be2c16fa7be812e5bbc4dcbeb20c7094aafe
wlodarrrr <mateuszwlodarczyk@live.com>
2023-06-28 16:58:35 +0200

    game class introduction

--- src/App.js
+++ src/App.js
@@ -1,19 +1,13 @@
 import './App.css';
 import Playground from './components/Playground/Playground';
+import Game from './domain/Game';
 
 function App() {
-  const values = [
-    { id: 0, colors: [2, 1, 6, 3] },
-    { id: 1, colors: [1, 1, 3, 3] },
-    { id: 2, colors: [7, 2, 6, 3] },
-    { id: 3, colors: [5, 1, 4, 3] },
-    { id: 4, colors: [5, 1, 4, 3] },
-    { id: 5, colors: [5, 1, 4, 3] },
-    { id: 6, colors: [5, 1, 4, 3] }
-  ];
+  let game = new Game();
+  game.reset(14,2);
   return (
     <div className="App">
-      <Playground values={values} />
+      <Playground game = {game} />
     </div>
   );
 }
--- src/components/Playground/Playground.js
+++ src/components/Playground/Playground.js
@@ -4,15 +4,14 @@ import Flask from '../Flask/Flask';
 
 class Playground extends React.Component {
 
-    state = { values: this.props.values, clicked: null }
+    state = { game: this.props.game, clicked: null }
 
     clicked = id => {
-        console.log("clicked");
         return this.setState({ clicked: id });
     };
 
     render() {
-        const flasks = this.state.values.map(row => { return <Flask key={row.id} colors={row.colors} selected={this.state.clicked === row.id} onClick={() => this.clicked(row.id)} /> });
+        const flasks = this.state.game.values.map(row => { return <Flask key={row.id} colors={row.colors} selected={this.state.clicked === row.id} onClick={() => this.clicked(row.id)} /> });
 
         return <div className='playground'>
             {flasks}
